fd05a623874c49bd923a38a43175af5b354f1746
machamp0714 <rateliasudaisuki@gmail.com>
2020-09-28 23:58:50 +0900

    ecs events roleを作成

--- service/sample_batch.tf
+++ service/sample_batch.tf
@@ -5,4 +5,5 @@ resource "aws_ecs_task_definition" "sample_batch" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions     = file("./print_date_container.json")
+  execution_role_arn       = module.ecs_events_role.iam_role_arn
 }
